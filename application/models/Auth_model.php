e3520e217e231e7a5f57b5046670a0540576b243
Zulyantara <zulyantara.tara@gmail.com>
2017-11-04 22:15:06 +0700

    setting login

---
+++ application/models/Auth_model.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Auth_model extends CI_Model
+{
+    /*
+     * @author zulyantara <zulyantara@gmail.com>
+     * @copyright copyright 2016 zulyantara
+     */
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function validate($username, $userpassword)
+    {
+        // $hashAndSalt = $this->get_password($username);
+        // $password_hash = password_hash($userpassword, PASSWORD_BCRYPT, array("cost" => 12));
+
+        $this->db->select("u.id, u.users_name, u.users_password, u.users_email, u.users_ur, ur.ur_ket");
+        $this->db->join('users_role ur','u.users_ur=ur.id','left');
+        $this->db->where('ur.users_name', $username);
+        $this->db->where("u.users_active", 1);
+        $query = $this->db->get('users u');
+        // echo $this->db->last_query();exit;
+        $row_user = $query->num_rows() > 0 ? $query->row() : FALSE;
+        if ($row_user !== FALSE)
+        {
+            if (password_verify($userpassword,$row_user->user_password))
+            {
+                return $query->row();
+                // return array("user_id"=>$row_user->user_id,"user_name"=>$row_user->user_name,"user_email"=>$row_user->user_email);
+            }
+            else
+            {
+                return FALSE;
+            }
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+}
--- application/views/login/home.php
+++ application/views/login/home.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminLTE 2 | Log in</title>
+        <title><?php echo SITE_TITLE; ?> | Login</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -38,7 +38,7 @@
 
                 <form action="<?php echo site_url("auth/validate_credential"); ?>" method="post">
                     <div class="form-group has-feedback">
-                        <input type="text" class="form-control" placeholder="User Name" name="txt_user_name">
+                        <input type="text" class="form-control" placeholder="User Name" name="txt_user_name" autofocus="autofocus">
                         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                     </div>
 
@@ -80,9 +80,8 @@
         <!-- /.login-box -->
 
         <!-- jQuery 3 -->
-        <script src="assets/adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="<?php echo base_url("assets/adminlte/bower_components/jquery/dist/jquery.min.js"); ?>"></script>
         <!-- Bootstrap 3.3.7 -->
-        <script src="assets/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-        <!-- iCheck -->
+        <script src="<?php echo base_url("assets/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"); ?>"></script>
     </body>
 </html>
