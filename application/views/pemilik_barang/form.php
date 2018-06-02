f4e93d31e0353a77f87f04e3d04dcc3b7b87cbde
zulyantara <zulyantara@gmail.com>
2018-06-03 04:18:03 +0700

    tambah modul pemilik barang

---
+++ application/views/pemilik_barang/form.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$pb_id = isset($res_pb) ? $res_pb->pb_id : "";
+$pb_kode = isset($res_pb) ? $res_pb->pb_kode : "";
+$pb_ket = isset($res_pb) ? $res_pb->pb_ket : "";
+$btn = isset($res_pb) ? "ubah" : "simpan";
+?>
+
+<div class="box box-success">
+  <div class="box-header with-border">
+    <h3 class="box-title"><?php echo isset($bt) ? $bt : ""; ?></h3>
+    <div class="box-tools pull-right">
+      <?php echo isset($pr) ? $pr : "";?>
+    </div>
+  </div>
+
+  <div class="box-body">
+
+    <form class="form-horizontal" action="<?php echo site_url($modul.'/simpan'); ?>" method="post">
+      <input type="hidden" name="pb_id" value="<?php echo $pb_id; ?>">
+      <div class="form-group">
+        <label for="pb_kode" class="control-label col-sm-2">Kode</label>
+        <div class="col-sm-10">
+          <div class="row">
+            <div class="col-sm-4">
+              <input type="text" name="pb_kode" id="pb_kode" value="<?php echo $pb_kode; ?>" placeholder="Kode" class="form-control" autofocus="autofocus" required="required" autocomplete="off">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="pb_ket" class="control-label col-sm-2">Ket</label>
+        <div class="col-sm-10">
+          <div class="row">
+            <div class="col-sm-4">
+              <input type="text" name="pb_ket" id="pb_ket" value="<?php echo $pb_ket; ?>" placeholder="Ket" class="form-control" required="required" autocomplete="off">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" name="simpan" value="<?php echo $btn; ?>" class="btn btn-primary btn-flat">Simpan</button>
+        </div>
+      </div>
+    </form>
+
+  </div>
+</div>
